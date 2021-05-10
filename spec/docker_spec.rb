c1b3e31da096cffb7c770765392ac29e75eca38e
Cahill <cahmason@a483e7926e96.ant.amazon.com>
2021-05-10 10:46:54 -0400

    remove test

--- spec/docker_spec.rb
+++ spec/docker_spec.rb
@@ -11,7 +11,7 @@ describe 'Dockerfile' do
     set :docker_image, image.id
   end
 
-  describe user('c7n') do
-    it { should exist }
-  end
+  # describe user('c7n') do
+  #   it { should exist }
+  # end
 end
